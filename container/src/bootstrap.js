6d74f82801c78a65c70e577780d647c77ffe1b6e
Bojan Sibar <bojansibar@Bojans-MacBook-Pro.local>
2021-01-11 14:22:57 +0100

    Chnage console log, add develop branch to repo

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,7 +1,7 @@
 import { mount as productsMount } from "products/ProductsIndex";
 import { mount as cartMount } from "cart/CartShow";
 
-console.log("Container!");
+console.log("Container app log");
 
 productsMount(document.querySelector("#my-products"));
 cartMount(document.querySelector("#my-cart"));
